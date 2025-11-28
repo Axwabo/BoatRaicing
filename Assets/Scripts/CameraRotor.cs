7aaa7518fc6bf16a251bbd0ed1e2d9dddf0b8114
Axwabo <axwabo@gmail.com>
2025-11-28 17:36:05 +0100

    manual boat control

---
+++ Assets/Scripts/CameraRotor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public sealed class CameraRotor : MonoBehaviour
+{
+
+    private const float Sensitivity = 0.1f;
+
+    private Transform _camera;
+
+    private Transform _player;
+
+    private void Awake()
+    {
+        _camera = transform;
+        _player = _camera.parent;
+    }
+
+    private void Update()
+    {
+        var delta = InputSystem.actions["Look"].ReadValue<Vector2>();
+        _camera.Rotate(-delta.y * Sensitivity, 0, 0);
+        _player.Rotate(0, delta.x * Sensitivity, 0);
+    }
+
+}
---
+++ Assets/Scripts/ManualBoatControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public sealed class ManualBoatControl : MonoBehaviour
+{
+
+    private Rigidbody _rb;
+
+    private void Awake() => _rb = GetComponent<Rigidbody>();
+
+    private void Update()
+    {
+        var move = InputSystem.actions["Move"].ReadValue<Vector2>();
+        if (move.y != 0)
+            _rb.AddRelativeForce((move.y < 0 ? Vector3.back : Vector3.forward) * 5);
+        if (move.x < 0)
+            _rb.AddRelativeTorque(0, -3, 0);
+        else if (move.x > 0)
+            _rb.AddRelativeTorque(0, 3, 0);
+    }
+
+}
