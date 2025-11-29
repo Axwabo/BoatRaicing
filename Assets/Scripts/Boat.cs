b1983cc3e7b7e7b63c00029c2e084aa97cdf0310
Axwabo <axwabo@gmail.com>
2025-11-29 14:43:13 +0100

    nerf "braking"

--- Assets/Scripts/Boat.cs
+++ Assets/Scripts/Boat.cs
@@ -7,6 +7,8 @@ public sealed class Boat : MonoBehaviour
 
     private Rigidbody _rb;
 
+    private Transform _t;
+
     [SerializeField]
     private float force = 1;
 
@@ -19,20 +21,28 @@ public sealed class Boat : MonoBehaviour
     [SerializeField]
     private float accelerationMultiplierRatio = 0.5f;
 
-    private void Awake() => _rb = GetComponent<Rigidbody>();
+    [SerializeField]
+    private float decelerationRatio = 0.3f;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _t = transform;
+    }
 
     public void Row(Vector2 move)
     {
         var forwards = Math.Sign(move.y);
         var sideways = Math.Sign(move.x);
-        if (forwards != 0)
-        {
-            var multiplier = Mathf.Min(1, _rb.linearVelocity.MagnitudeIgnoreY() / accelerationMultiplierTarget) * accelerationMultiplierRatio + 1;
-            _rb.AddRelativeForce(0, 0, forwards * force * multiplier * Time.fixedDeltaTime);
-        }
-
         if (sideways != 0)
             _rb.AddTorque(0, sideways * torque * Time.fixedDeltaTime, 0);
+        if (forwards == 0)
+            return;
+        var linear = _rb.linearVelocity;
+        var multiplier = Math.Sign(_t.InverseTransformDirection(linear).z) == forwards
+            ? Mathf.Min(1, linear.MagnitudeIgnoreY() / accelerationMultiplierTarget) * accelerationMultiplierRatio + 1
+            : decelerationRatio;
+        _rb.AddRelativeForce(0, 0, forwards * force * multiplier * Time.fixedDeltaTime);
     }
 
 }
