88b2ba257830b3cd2ca24344fa0c09ad39775258
Axwabo <axwabo@gmail.com>
2025-11-29 17:03:53 +0100

    cutscenes

---
+++ Assets/Scripts/Maps/CutsceneSequence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Maps
+{
+
+    [Serializable]
+    public sealed class CutsceneSequence
+    {
+
+        public Transform from;
+
+        public Transform to;
+
+        public float duration = 5;
+
+    }
+
+}
---
+++ Assets/Scripts/Maps/Starter.cs
@@ -0,0 +1,75 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Maps
+{
+
+    public sealed class Starter : MonoBehaviour
+    {
+
+        private const float Waiting = 10;
+        private const float Countdown = 3;
+
+        public static float TimeToStart { get; private set; }
+
+        private Phase _phase;
+
+        private int _index = -1;
+
+        private float _delay;
+
+        [SerializeField]
+        private Transform cam;
+
+        [SerializeField]
+        private CutsceneSequence[] cutscenes;
+
+        private void Start() => TimeToStart = cutscenes.Sum(e => e.duration) + Waiting + Countdown;
+
+        private void Update()
+        {
+            TimeToStart -= Time.deltaTime;
+            _delay -= Time.deltaTime;
+            switch (_phase)
+            {
+                case Phase.Cutscenes when _delay <= 0:
+                    if (++_index < cutscenes.Length)
+                    {
+                        _delay = cutscenes[_index].duration;
+                        goto case Phase.Cutscenes;
+                    }
+
+                    _phase = Phase.Waiting;
+                    _delay += Waiting;
+                    break;
+                case Phase.Cutscenes:
+                    var sequence = cutscenes[_index];
+                    sequence.from.GetPositionAndRotation(out var fromPos, out var fromRot);
+                    sequence.to.GetPositionAndRotation(out var toPos, out var toRot);
+                    cam.SetPositionAndRotation(
+                        Vector3.Lerp(toPos, fromPos, _delay / sequence.duration),
+                        Quaternion.Lerp(toRot, fromRot, _delay / sequence.duration)
+                    );
+                    break;
+                case Phase.Waiting when _delay <= 0:
+                    _phase = Phase.CountingDown;
+                    _delay += Countdown;
+                    break;
+                case Phase.CountingDown when _delay <= 0:
+                    Destroy(this);
+                    break;
+            }
+        }
+
+        private enum Phase
+        {
+
+            Cutscenes,
+            Waiting,
+            CountingDown
+
+        }
+
+    }
+
+}
