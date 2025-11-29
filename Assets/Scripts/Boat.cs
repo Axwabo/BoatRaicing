1730f438964f8b40f4d12644f6bdfe4eae363223
Axwabo <axwabo@gmail.com>
2025-11-29 08:08:24 +0100

    Boat behavior

---
+++ Assets/Scripts/Boat.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public sealed class Boat : MonoBehaviour
+{
+
+    private Rigidbody _rb;
+
+    private void Awake() => _rb = GetComponent<Rigidbody>();
+
+    public void Row(bool forwards, bool backwards, bool left, bool right)
+    {
+        if (forwards && backwards && left && right)
+            return;
+        Debug.Log($"{forwards} {backwards} {left} {right}");
+        var leftAmount = left ? 1 : 0;
+        var rightAmount = right ? 1 : 0;
+        // TODO
+    }
+
+}
--- Assets/Scripts/ManualBoatControl.cs
+++ Assets/Scripts/ManualBoatControl.cs
@@ -1,24 +1,18 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Boat))]
 public sealed class ManualBoatControl : MonoBehaviour
 {
 
-    private Rigidbody _rb;
+    private Boat _boat;
 
-    private void Awake() => _rb = GetComponent<Rigidbody>();
+    private void Awake() => _boat = GetComponent<Boat>();
 
     private void FixedUpdate()
     {
         var move = InputSystem.actions["Move"].ReadValue<Vector2>();
-        if (move.y != 0)
-            _rb.AddRelativeForce((move.y < 0 ? Vector3.back : Vector3.forward), ForceMode.Acceleration);
-        _rb.maxLinearVelocity = 1;
-        if (move.x < 0)
-            _rb.AddTorque(0, -1, 0, ForceMode.Acceleration);
-        else if (move.x > 0)
-            _rb.AddTorque(0, 1, 0, ForceMode.Acceleration);
+        _boat.Row(move.y > 0, move.y < 0, move.x < 0, move.x > 0);
     }
 
 }
