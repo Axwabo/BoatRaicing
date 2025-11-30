eaed006448aa0b927b2e38fa9b4b63645622cf98
Axwabo <axwabo@gmail.com>
2025-11-30 17:11:04 +0100

    random material picker

---
+++ Assets/Scripts/Bots/RandomMaterial.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Bots
+{
+
+    [RequireComponent(typeof(MeshRenderer))]
+    public sealed class RandomMaterial : MonoBehaviour
+    {
+
+        [SerializeField]
+        private Material[] materials;
+
+        private void Awake() => GetComponent<MeshRenderer>().sharedMaterial = materials[Random.Range(0, materials.Length)];
+
+    }
+
+}
