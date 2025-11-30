278990e1817bd91bbd8aaa7b03b1c6fe7f76a5d5
Axwabo <axwabo@gmail.com>
2025-11-30 08:54:02 +0100

    ReloadSceneButton

---
+++ Assets/Scripts/Menu/ReloadSceneButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace Menu
+{
+
+    public sealed class ReloadSceneButton : ButtonBase
+    {
+
+        protected override void Click() => SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
+}
