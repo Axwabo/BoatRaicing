9017c4f7319fbf6243e03ec9fa55bada15f316cc
Axwabo <axwabo@gmail.com>
2025-11-30 11:18:30 +0100

    screenshot capture script

---
+++ Assets/Editor/Screenshot.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+
+    public static class Screenshot
+    {
+
+        [MenuItem("Assets/Screenshot")]
+        public static void Grab() => ScreenCapture.CaptureScreenshot("Assets/Screenshot.png", 2);
+
+    }
+
+}
