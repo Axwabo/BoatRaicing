25e8ca77cc474a2b4da7e7f6ee5aa05c6ed3a3a4
Axwabo <axwabo@gmail.com>
2025-11-30 17:06:34 +0100

    lock in

---
+++ Assets/Scripts/LockInHandler.cs
@@ -0,0 +1,30 @@
+﻿using Menu;
+using UnityEngine;
+
+public sealed class LockInHandler : MonoBehaviour
+{
+
+    private MeshRenderer[] _renderers;
+
+    private AudioSource[] _sources;
+
+    private bool _isLocked;
+
+    private void Awake()
+    {
+        _renderers = GetComponentsInChildren<MeshRenderer>();
+        _sources = GetComponentsInChildren<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (_isLocked == LockIn.Locked)
+            return;
+        _isLocked = LockIn.Locked;
+        foreach (var meshRenderer in _renderers)
+            meshRenderer.enabled = !_isLocked;
+        foreach (var source in _sources)
+            source.mute = _isLocked;
+    }
+
+}
---
+++ Assets/Scripts/Menu/LockIn.cs
@@ -0,0 +1,19 @@
+﻿namespace Menu
+{
+
+    public sealed class LockIn : Checkbox
+    {
+
+        public static bool Locked { get; private set; }
+
+        protected override bool Value
+        {
+            set => Locked = value;
+        }
+
+        protected override bool DefaultValue => false;
+        protected override string Key => "Lock In";
+
+    }
+
+}
