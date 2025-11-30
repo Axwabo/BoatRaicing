77ec6e594bf9a7dfbc2a1789efa6d437b9b2d9c5
Axwabo <axwabo@gmail.com>
2025-11-30 23:46:51 +0100

    80 + apply time from main

--- Assets/Scripts/Extensions.cs
+++ Assets/Scripts/Extensions.cs
@@ -7,6 +7,6 @@ public static class Extensions
     public static float MagnitudeIgnoreY(this Vector3 vector3) => Mathf.Sqrt(vector3.x * vector3.x + vector3.z * vector3.z);
 
     public static void Set(this AudioMixer mixer, string param, float percentage)
-        => mixer.SetFloat(param, Mathf.Approximately(0, percentage) ? -18 : 20 * Mathf.Log10(percentage));
+        => mixer.SetFloat(param, Mathf.Approximately(0, percentage) ? -80 : 20 * Mathf.Log10(percentage));
 
 }
