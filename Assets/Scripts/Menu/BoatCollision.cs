5fcea83b95ae950d9f7b168d9369d94a6508a652
Axwabo <axwabo@gmail.com>
2025-11-30 23:57:28 +0100

    boat collision

---
+++ Assets/Scripts/Menu/BoatCollision.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Menu
+{
+
+    public sealed class BoatCollision : Checkbox
+    {
+
+        protected override bool Value
+        {
+            set => Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Boat"), LayerMask.NameToLayer("Boat"), !value);
+        }
+
+        protected override bool DefaultValue => false;
+        protected override string Key => "Boat Collision";
+
+    }
+
+}
