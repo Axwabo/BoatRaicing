9aa0f2840fbfaf1730435f13698c148e6b336b64
Axwabo <axwabo@gmail.com>
2025-11-30 10:56:43 +0100

    pause button

---
+++ Assets/Scripts/Menu/Pause.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Menu
+{
+
+    public sealed class Pause : ButtonBase
+    {
+
+        private TextMeshProUGUI _text;
+
+        private void Start() => _text = GetComponentInChildren<TextMeshProUGUI>();
+
+        protected override void Click() => Set(!AudioListener.pause);
+
+        private void Set(bool paused)
+        {
+            AudioListener.pause = paused;
+            Time.timeScale = paused ? 0 : 1;
+            _text.text = paused ? "Continue" : "Pause";
+        }
+
+        private void OnDestroy() => Set(false);
+
+    }
+
+}
