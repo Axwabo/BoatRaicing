161be707863019ccd5941420e6b1e0151e6d76ee
Axwabo <axwabo@gmail.com>
2025-11-30 15:10:18 +0100

    scaffold BotBoatControl

---
+++ Assets/Scripts/Bots/BotBoatControl.cs
@@ -0,0 +1,29 @@
+﻿using Maps;
+using UnityEngine;
+
+namespace Bots
+{
+
+    [RequireComponent(typeof(Boat))]
+    public sealed class BotBoatControl : MonoBehaviour
+    {
+
+        private Boat _boat;
+
+        private Transform _t;
+
+        private void Awake()
+        {
+            _boat = GetComponent<Boat>();
+            _t = transform;
+        }
+
+        private void FixedUpdate()
+        {
+            if (Starter.TimeToStart > 0)
+                return;
+        }
+
+    }
+
+}
